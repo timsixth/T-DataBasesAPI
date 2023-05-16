9fa419c2fe73bc552184f01c5f58d21fd435fdab
timsixth <timsixth@op.pl>
2023-05-16 09:17:26 +0200

    Added QueryBuilder

---
+++ src/main/java/pl/timsixth/databasesapi/database/query/QueryBuilder.java
@@ -0,0 +1,160 @@
+package pl.timsixth.databasesapi.database.query;
+
+import java.util.Map;
+
+/**
+ * Creates queries
+ */
+public final class QueryBuilder {
+    private final StringBuilder query = new StringBuilder();
+
+    /**
+     * @param table        the table name
+     * @param columnsNames columns to queried
+     * @return QueryBuilder
+     */
+    public QueryBuilder select(String table, String... columnsNames) {
+        query.append("SELECT ");
+
+        setColumns(columnsNames);
+
+        from(table);
+
+        return this;
+    }
+
+    /**
+     * Creates select query with all columns
+     *
+     * @param table the table name
+     * @return QueryBuilder
+     */
+    public QueryBuilder selectAll(String table) {
+        return select(table, "*");
+    }
+
+    /**
+     * Creates the delete query
+     *
+     * @param table        the table name
+     * @param columnsNames columns to queried
+     * @return QueryBuilder
+     */
+    public QueryBuilder delete(String table, String... columnsNames) {
+        query.append("DELETE ");
+
+        setColumns(columnsNames);
+
+        from(table);
+
+        return this;
+    }
+
+    /**
+     * Creates delete query with all columns
+     *
+     * @param table the table name
+     * @return QueryBuilder
+     */
+    public QueryBuilder deleteAll(String table) {
+        return delete(table, "*");
+    }
+
+    /**
+     * Adds where condition to query
+     *
+     * @param whereCondition condition
+     * @return QueryBuilder
+     */
+    public QueryBuilder where(String whereCondition) {
+        query.append(" WHERE ");
+
+        query.append(whereCondition);
+
+        return this;
+    }
+
+    /**
+     * Creates inesrt into query
+     *
+     * @param table the table name
+     * @param data  the data to insert
+     * @return QueryBuilder
+     */
+    public QueryBuilder insert(String table, Object... data) {
+        if (data.length == 0) throw new IllegalStateException("data can not be empty");
+
+        query.append("INSERT INTO ").append(table);
+
+        query.append(" VALUES(");
+
+        for (Object datum : data) {
+            if (datum instanceof String)
+                query.append("'").append(datum).append("'").append(",");
+            else query.append(datum).append(",");
+
+        }
+
+        query.deleteCharAt(query.lastIndexOf(","));
+
+        query.append(")");
+
+        return this;
+    }
+
+    /**
+     * Creates update query
+     *
+     * @param table the table name
+     * @param data  the data to update
+     * @return QueryBuilder
+     */
+    public QueryBuilder update(String table, Map<String, Object> data) {
+        if (data.isEmpty()) throw new IllegalStateException("data can not be empty");
+
+        query.append("UPDATE ").append(table);
+
+        data.forEach((key, value) -> {
+            if (value instanceof String)
+                query.append(" SET ").append(key)
+                        .append(" = ").append("'").append(value).append("'").append(",");
+            else query.append(" SET ").append(key)
+                    .append(" = ").append(value).append(",");
+        });
+
+        query.deleteCharAt(query.lastIndexOf(","));
+
+        return this;
+    }
+
+    /**
+     * Adds from do query
+     *
+     * @param table the table name
+     */
+    private void from(String table) {
+        query.append(" FROM ").append(table);
+    }
+
+    /**
+     * Sets columns
+     *
+     * @param columnsNames columns to queried
+     */
+    private void setColumns(String[] columnsNames) {
+        if (columnsNames.length == 0) throw new IllegalStateException("columnsNames can not be empty");
+
+        for (String columnName : columnsNames) {
+            query.append(columnName).append(",");
+        }
+
+        query.deleteCharAt(query.lastIndexOf(","));
+    }
+
+    /**
+     * @return built query
+     */
+    public String build() {
+        return query.toString();
+    }
+}
---
+++ src/test/java/pl/timsixth/databasesapi/tests/QueryBuilderTest.java
@@ -0,0 +1,78 @@
+package pl.timsixth.databasesapi.tests;
+
+import org.junit.Test;
+import pl.timsixth.databasesapi.database.query.QueryBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+public class QueryBuilderTest {
+
+    @Test
+    public void shouldGenerateSelectQueryWithColumns() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String select = queryBuilder.select("users", "username", "password").build();
+
+        assertEquals("SELECT username,password FROM users", select);
+    }
+
+    @Test
+    public void shouldGenerateSelectQueryWithAllColumns() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String select = queryBuilder.selectAll("users").build();
+
+        assertEquals("SELECT * FROM users", select);
+    }
+
+    @Test
+    public void shouldGenerateDeleteQuery() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String query = queryBuilder.delete("users", "password", "username").build();
+
+        assertEquals("DELETE password,username FROM users", query);
+    }
+
+    @Test
+    public void shouldGenerateDeleteQueryWithAllColumns() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String query = queryBuilder.deleteAll("users").build();
+
+        assertEquals("DELETE * FROM users", query);
+    }
+
+    @Test
+    public void shouldGenerateDeleteQueryWithAllColumnsWithWhere() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String query = queryBuilder.deleteAll("users").where("username = 'test'").build();
+
+        assertEquals("DELETE * FROM users WHERE username = 'test'", query);
+    }
+
+    @Test
+    public void shouldGenerateInsertIntoQuery() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        String query = queryBuilder.insert("users", null, "user", 12).build();
+
+        assertEquals("INSERT INTO users VALUES(null,'user',12)", query);
+    }
+
+    @Test
+    public void shouldGenerateUpdateQuery() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("username", "test");
+        data.put("age", 12);
+
+        String query = queryBuilder.update("users", data).build();
+
+        assertEquals("UPDATE users SET age = 12, SET username = 'test'", query);
+    }
+}
